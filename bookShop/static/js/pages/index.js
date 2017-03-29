ca51bf0160e0d749c1f3ff5197e66d46f368e856
xinzikang <18519127194@163.com>
2017-03-29 23:37:46 +0800

    3-29

--- bookShop/static/js/pages/index.js
+++ bookShop/static/js/pages/index.js
@@ -11,21 +11,44 @@ $.ajax({
       data:{
         window_width: window_width,
         double_width:'width:'+window_width*2+'px',
+        single_width:window_width+'px',
+        banner_height:window_width*0.36+'px',
         top: d.items[0],
         hot: d.items[1],
         recommend: d.items[2],
+        recommend_show:[],
+        recommend_min:0,
+        recommend_max:5,
         female: d.items[3],
         male: d.items[4],
         free: d.items[5],
         bookcase: []
+      },
+      watch:{
+        recommend_min(){
+          this.showChange()
+        }
+      },
+      created(){
+        this.showChange()
+      },
+      beforeMounted(){
+
       },
       mounted() {
         let that = this;
         let oBody = $('body');
         let oBookCase = $('#book-case');
         let oIndexWarpper = $('.index-warpper');
+        let oTabBar = $('#tab_bar')[0];
+        let oTabIndex = $('#index_header')[0];
+        let oTabBookCase = $('#bookcase_header')[0];
         let iBookCaseHei = oBookCase.height();
         let iIndexHei = oIndexWarpper.height();
+        let oBannerContainer = $('.banner-swiper-container');
+        let oBannerLi = $('.banner-swiper-container').find('li');
+        console.log(oBannerLi)
+        oBannerContainer.width(oBannerLi.length*this.window_width);
         let mySwiper = new Swiper ('.swiper-container', {
           direction: 'horizontal',
           loop: false,
@@ -42,16 +65,42 @@ $.ajax({
               oBookCase.show();
             }
             if(mySwiper.activeIndex === 1){ //当前活动块的索引为0表示bookcase展示
-             console.log('书架也')
+             console.log('书架也');
+              oTabBar.style.left = '64%';
+              oTabIndex.className = '';
+              oTabBookCase.className = 'active';
              oIndexWarpper.height(iBookCaseHei);
+
              } else{
              console.log('index也');
+              oTabBar.style.left = '26%';
+              oTabIndex.className = 'active';
+              oTabBookCase.className = '';
              oIndexWarpper.height(iIndexHei);
              }
+          },
+          onSlideChangeStart(){
+            if(mySwiper.activeIndex === 1){ //当前活动块的索引为0表示bookcase展示
+              oTabBar.style.left = '64%';
+              oTabIndex.className = '';
+              oTabBookCase.className = 'active';
+
+            } else{
+              oTabBar.style.left = '26%';
+              oTabIndex.className = 'active';
+              oTabBookCase.className = '';
+            }
           }
         });
+        let bannerSwiper = new Swiper('.banner-swiper-container',{
+          direction: 'horizontal',
+          loop: true
+        })
       },
       methods: {
+        showChange(){
+          this.recommend_show = this.recommend.data.data.slice(this.recommend_min,this.recommend_max);
+        },
         gotoDetail(id) {
           location.href = '/detail';
           $.ajax({
@@ -71,6 +120,18 @@ $.ajax({
         },
         gotoSearch(){
           location.href = '/search';
+        },
+        tabToMale() {
+          this.recommend_min=0;
+          this.recommend_max=5;
+          this.$refs.maleBtn.className = 'active';
+          this.$refs.femaleBtn.className = '';
+        },
+        tabToFemale() {
+          this.recommend_min=15;
+          this.recommend_max=20;
+          this.$refs.maleBtn.className = '';
+          this.$refs.femaleBtn.className = 'active';
         }
       }
     });
--- bookShop/static/js/pages/search.js
+++ bookShop/static/js/pages/search.js
@@ -5,13 +5,22 @@
 new Vue({
   el:"#search-root",
   data: {
-    search: [],
-    enpty: false
+    search_text: '',
+    search_result:[],
+    li_val:['如果蜗牛有爱情','择天记','一夜弃妃','诛仙','豪门小老婆','异能小农民','武动乾坤','别那么骄傲']
   },
   methods: {
-    doSearch() {
-      let keyword = $('#searchText').val();
+    doSearch(dd) {
+      if(dd !== ''){
+        var keyword = dd;
+      } else{
+        var keyword = this.search_text;
+      }
       let that = this;
+      if(!keyword){ // 如果搜索框没有内容,置空
+        this.search_result = [];
+        return;
+      }
       $.ajax({
         url: '/ajax/search',
         dataType: 'json',
@@ -19,10 +28,48 @@ new Vue({
           keyword : keyword
         },
         success: function (d) {
-          console.log(d)
+          that.search_text = '';
+          that.search_result = d.items;
+          console.log(that.search_result);
         },
         error: function (d) {
           console.log(d)
+        }
+      })
+    },
+    /*doLiSearch(liValue) { // bug:传参与不传参的问题：不传参的时候这个参数是ev，所以不传参的时候需要传入一个空字符串
+      let keyword = liValue;
+      let that = this;
+      console.log(11,keyword,22);
+      $.ajax({
+        url: '/ajax/search',
+        dataType: 'json',
+        data:{
+          keyword : keyword
+        },
+        success: function (d) {
+          that.search_result = d.items;
+          console.log(that.search_result);
+        },
+        error: function (d) {
+          console.log(d)
+        }
+      })
+    },*/
+    gotoDetail(id) {
+      location.href = '/detail';
+      $.ajax({
+        url:'/ajax/detail',
+        data:{
+          "id":id
+        },
+        dataType: 'json',
+        success: function (d) {
+          console.log(d,'请求成功');
+          localStorage.setItem('bookId',id);
+        },
+        error: function (d) {
+          console.log(d,'请求失败');
         }
       })
     }
